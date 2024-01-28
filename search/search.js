967f260f21a33a87eeffb14e9eb5e4a12098e1bb
Richard Kužela <richard@kanshen.click>
2024-01-28 18:08:34 +0100

    Added a prototype search page
    Note: THIS IS STILL IN HEAVY DEVELOPMENT AND DOES NOT WORK YET

---
+++ search/search.js
@@ -0,0 +1,27 @@
+async function placeholderReplace() {
+    var songs = await fetch("../s/songs.json").then(response => response.json());
+    var artists = await fetch("../a/artists.json").then(response => response.json());
+    var releases = await fetch("../r/releases.json").then(response => response.json());
+
+    var prompts = [];
+    for (const artist in artists) {
+        prompts.push(artist);
+        if (artist !== "FlyJungeMraza") {
+            prompts.push(artists[artist][0].portfolioLINK);
+        }
+    }
+    for (const release of releases) {
+        prompts.push(release.id);
+        prompts.push(release.title);
+    }
+    for (const song in songs) {
+        prompts.push(song);
+        prompts.push(songs[song].title);
+    }
+
+    setInterval(function() {
+        var input = document.querySelector('search>input[type="text"]');
+            var randomPrompt = prompts[Math.floor(Math.random() * prompts.length)];
+            input.placeholder = randomPrompt;
+    }, 1000);
+}
