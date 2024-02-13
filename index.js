e7f99d227013417deaaf3f94696a483de09f1051
Richard Kužela <richard@kanshen.click>
2024-02-14 00:00:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ function setUpcomingRelease(releases, artists, songs) {
     function updateCountdown(endTime) {
         const now = new Date();
         const timezoneOffset = now.getTimezoneOffset() * 60000;
-        const localNow = new Date(now.valueOf() + timezoneOffset).valueOf();
+        const localNow = new Date(now.valueOf() - timezoneOffset).valueOf();
         const difference = new Date(endTime).valueOf() - localNow;
       
         if (difference <= 0) {
